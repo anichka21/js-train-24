ed0567b1a2b508ddfb0b83fa93fc38c70e5be867
anichka21 <anashnaidruk@gmail.com>
2024-04-25 16:04:00 +0300

    js-train-24

--- 3.js
+++ 3.js
@@ -17,7 +17,34 @@ function task3() {
   // Функція finally викликається після того, як проміс вирішено або відхилено
   // Вона використовується для виконання дій, які повинні виконуватися незалежно від того, чи було проміс вирішено чи відхилено
   // В нашому випадку ми просто виводимо повідомлення, "Завдання 3 завершено"
+  function promise1() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Проміс 1 виконано") 
+      }, 1000);
+      });
+  }
+  function promise2() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject("Проміс 1 виконано") 
+      }, 2000);
+      });
+  }
+  Promise.allSettled([promise1(), promise2()])
+  .then(results => {
+    results.forEach(result => {
+      if (result.status === "fulfilled") {
+        console.log("Проміс вирішено:", result.value);
+      }
+       else {
+        console.log("Проміс відхилено:", result.reason);
+      }
+    });
+    })
+    .finally(() => {
+      console.log("Завдання 3 завершено");
+      });
 }
-
 // Викликаємо функцію task3
 task3();
--- 9.js
+++ 9.js
@@ -7,6 +7,17 @@ function task9() {
   // Викликаємо функцію fetchWithError
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
+  function fetchWithError() {
+    const errorMessage = "Помилка при з'єднанні з сервером";
+    Promise.reject(errorMessage)
+  };
+  fetchWithError() 
+  .then((data) => {
+    console.log(data)
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 }
 
 // Викликаємо функцію task10
